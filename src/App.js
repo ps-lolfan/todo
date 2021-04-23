81d20b3eb1d5713e97ebfc7249ff7953019dea82
praveensinnur <pbsinnur@gmail.com>
2021-04-24 00:10:43 +0530

    basic form setup done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import React, {useState} from 'react'
+import Form from './components/Form'
+import TodoList from './components/TodoList'
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  
   return (
     <div className="App">
       <header>
         <h1>Simple todo List </h1>
       </header>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,1 +1,38 @@
 import React from 'react'
+
+const Form = ({setInputText,inputText, todos, setTodos}) => {
+
+  //To handle input text
+  const inputTextHandler = e => {
+    setInputText(e.target.value)
+  }
+
+  //To handle submit text
+  const submitTodoHandler = (e) => {
+    e.preventDefault()
+    setTodos([
+      ...todos, {
+        text: inputText,
+        completed: false,
+        id: Math.random()*1000
+      }
+    ])
+  }
+    return (
+        <form>
+        <input onChange={inputTextHandler} type="text" className="todo-input" />
+        <button onClick={submitTodoHandler} className="todo-button" type="submit">
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
+    )
+}
+
+export default Form
